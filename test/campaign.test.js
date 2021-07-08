2de8b30ba08e784c36c3ae271323c41b711882d7
orhanors <orsorhan1@gmail.com>
2021-07-08 12:24:14 +0300

    completed tests

--- test/campaign.test.js
+++ test/campaign.test.js
@@ -89,4 +89,33 @@ describe("Campaigns", () => {
 			resultRequest.description
 		);
 	});
+
+	it("processes request", async () => {
+		await campaign.methods.contribute().send({
+			from: accounts[0],
+			value: web3.utils.toWei("10", "ether"),
+		});
+
+		await campaign.methods
+			.createRequest(
+				"Some description",
+				web3.utils.toWei("5", "ether"),
+				accounts[1]
+			)
+			.send({ from: accounts[0], gas: "1000000" });
+
+		await campaign.methods
+			.approveRequest(0)
+			.send({ from: accounts[0], gas: "1000000" });
+
+		await campaign.methods
+			.finalizeRequest(0)
+			.send({ from: accounts[0], gas: "1000000" });
+
+		let balance = await web3.eth.getBalance(accounts[1]);
+		balance = web3.utils.fromWei(balance, "ether");
+		balance = parseFloat(balance);
+
+		assert(balance > 104);
+	});
 });
